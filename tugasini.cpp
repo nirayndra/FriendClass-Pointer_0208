79a4cded79df3fc7a0fd7c111c64f83f24f81c4e
nirayndra <faadiajasmine@gmail.com>
2026-04-27 21:03:07 +0700

    menambah member

--- tugasini.cpp
+++ tugasini.cpp
@@ -4,4 +4,13 @@ using namespace std;
 
 class BelahKetupat;
 class LayangLayang{
+private:
+    double d1, d2, sisi1, sisi2;
+
+public:
+    void setD1(double pD1) { d1 = pD1; }
+    void setD2(double pD2) { d2 = pD2; }
+    void setSisi1(double pSisi1) { sisi1 = pSisi1; }
+    void setSisi2(double pSisi2) { sisi2 = pSisi2; }
+
 };
