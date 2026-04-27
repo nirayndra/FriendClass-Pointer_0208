25ad5413e1f57255e2c9aeb4fe913f0918cb65a3
nirayndra <faadiajasmine@gmail.com>
2026-04-27 21:03:35 +0700

    menambah proses penghitungan

--- tugasini.cpp
+++ tugasini.cpp
@@ -13,4 +13,16 @@ public:
     void setSisi1(double pSisi1) { sisi1 = pSisi1; }
     void setSisi2(double pSisi2) { sisi2 = pSisi2; }
 
+    double hitungLuas() {
+        return 0.5 * d1 * d2;
+    }
+
+    double hitungKeliling() {
+        return 2 * (sisi1 + sisi2);
+    }
+
+    void outputData() {
+        cout << "Luas Layang-Layang: " << hitungLuas() << endl;
+        cout << "Keliling Layang-Layang: " << hitungKeliling() << endl;
+    }
 };
